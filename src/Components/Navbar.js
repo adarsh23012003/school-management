feb6829ef73faaa1e2d7eaf0b3a8ca4e5b175ed0
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-21 12:23:29 +0530

    Navbar Added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <div>
+        <nav className='p-2 bg-blue-400'>
+          <ul className='flex gap-2 font-serif justify-center'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
