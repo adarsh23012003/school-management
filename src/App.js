5cb1955ab8381818772f719bf79af1114aefa092
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-25 13:23:22 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../src/pages/Home";
 import About from "../src/pages/About";
 import NoPage from "../src/pages/NoPage";
+import Student from "../src/pages/Student";
+import StudentDetails from "../src/pages/StudentDetails";
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/student' element={<Student />} />
+        <Route path='/student/:studentId' element={<StudentDetails />} />
         <Route path='*' element={<NoPage />} />
       </Routes>
     </>
