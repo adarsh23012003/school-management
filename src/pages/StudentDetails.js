8f96af8285fa0cc1a3ea5504ac5b172feec05ed5
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-25 13:44:12 +0530

    one student data

--- src/pages/StudentDetails.js
+++ src/pages/StudentDetails.js
@@ -1,7 +1,48 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function StudentDetails() {
-  return <div>StudentDetails</div>;
+  const [data, setData] = useState({});
+  const { studentId } = useParams();
+
+  useEffect(() => {
+    function StudentDetail() {
+      axios
+        .get(`http://localhost:5000/student/${studentId}`)
+        .then(function (response) {
+          setData(response.data.student);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    StudentDetail();
+  }, []);
+
+  return (
+    <div className='flex justify-center'>
+      <div>
+        <img
+          className='rounded-full w-32 h-32 sm:w-40 sm:h-40 object-fill'
+          src={`http://localhost:5000/${data.image}`}
+          alt='Image'
+        />
+        <ul>
+          <li>{data.fullName}</li>
+          <li>{data.fatherName}</li>
+          <li>{data.motherName}</li>
+          <li>{data.address}</li>
+          <li>{data.email}</li>
+          <li>{data.mobileNumber}</li>
+          <li>{data.dob}</li>
+          <li>{data.category}</li>
+          <li>{data.gender}</li>
+          <li>{data.studentClass}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default StudentDetails;
