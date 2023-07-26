e97e2a2324cff300768570baa275ffc124bf8e25
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-26 14:02:24 +0530

    css changes for dropdown

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -61,28 +61,31 @@ function Student() {
     <>
       <div className='flex justify-center'>
         <div>
-          <form>
-            <label for='class'>Choose a class:</label>
-            <select
-              name='class'
-              id='class'
-              onChange={(e) => {
-                setClassDropdownValue(e.target.value);
-              }}
-              defaultValue=''
-            >
-              <option value=''>Select</option>
-              {classLists.map((element) => {
-                return (
-                  <>
-                    <option value={element}>{element}</option>
-                  </>
-                );
-              })}
-            </select>
-          </form>
-        </div>
-        <div>
+          {/* ************* Dropdown **************** */}
+          <div className='flex justify-center p-5'>
+            <form>
+              <label for='class'>Choose a class:</label>
+              <select
+                className='border-2 rounded-md p-0.5 focus:outline-blue-400'
+                name='class'
+                id='class'
+                onChange={(e) => {
+                  setClassDropdownValue(e.target.value);
+                }}
+                defaultValue=''
+              >
+                <option value=''>Select</option>
+                {classLists.map((element) => {
+                  return (
+                    <>
+                      <option value={element}>{element}</option>
+                    </>
+                  );
+                })}
+              </select>
+            </form>
+          </div>
+
           {classDropdownValue ? (
             <>
               <div>
@@ -94,54 +97,56 @@ function Student() {
                         navigate(`${element._id}`);
                       }}
                     >
-                      <div className='shadow-indigo-500/40 p-5 sm:flex sm:justify-between'>
-                        <div className='border-2 border-gray-700 p-2 flex justify-center'>
-                          <img
-                            src={`http://localhost:5000/${element.image}`}
-                            alt='flower Image'
-                            className='rounded-full w-32 h-32 sm:w-40 sm:h-40 object-fill border-red-900 border-2'
-                          />
-                        </div>
-                        <div className='border-2 border-red-700 p-2'>
-                          <div className='text-center items-center'>
-                            <h1 className='font-semibold'>
-                              {element.fullName}
-                            </h1>
-                            <p className='pt-2 text-sm'>
-                              {`${element.address.slice(0, 10)}...`}
-                            </p>
+                      <div>
+                        <div className='shadow-lg shadow-indigo-500/40 p-2 sm:flex sm:justify-between border-2 mb-5'>
+                          <div className='p-2 flex justify-center'>
+                            <img
+                              src={`http://localhost:5000/${element.image}`}
+                              alt='flower Image'
+                              className='rounded-full w-32 h-32 sm:w-32 sm:h-w-32 object-fill'
+                            />
                           </div>
-                          <div className='flex justify-between text-center'>
-                            <table className='p-1 border text-sm'>
-                              <tr>
-                                <td className='border'>
-                                  Gender
-                                  <p className='pt-2 text-xs'>
-                                    {element.gender}
-                                  </p>
-                                </td>
-                              </tr>
-                            </table>
-                            <table className='p-1 border text-sm'>
-                              <tr>
-                                <td className=' border'>
-                                  class
-                                  <p className='pt-2 text-xs'>
-                                    {element.studentClass}
-                                  </p>
-                                </td>
-                              </tr>
-                            </table>
-                            <table className='p-1 border text-sm'>
-                              <tr>
-                                <td className='border'>
-                                  Gender
-                                  <p className='pt-2 text-xs'>
-                                    {element.category}
-                                  </p>
-                                </td>
-                              </tr>
-                            </table>
+                          <div className='p-2'>
+                            <div className='text-center items-center'>
+                              <h1 className='font-semibold'>
+                                {element.fullName}
+                              </h1>
+                              <p className='pt-2 text-sm'>
+                                {`${element.address.slice(0, 10)}...`}
+                              </p>
+                            </div>
+                            <div className='flex justify-between text-center'>
+                              <table className='p-1 border text-sm'>
+                                <tr>
+                                  <td className='border'>
+                                    Gender
+                                    <p className='pt-2 text-xs'>
+                                      {element.gender}
+                                    </p>
+                                  </td>
+                                </tr>
+                              </table>
+                              <table className='p-1 border text-sm'>
+                                <tr>
+                                  <td className=' border'>
+                                    class
+                                    <p className='pt-2 text-xs'>
+                                      {element.studentClass}
+                                    </p>
+                                  </td>
+                                </tr>
+                              </table>
+                              <table className='p-1 border text-sm'>
+                                <tr>
+                                  <td className='border'>
+                                    Gender
+                                    <p className='pt-2 text-xs'>
+                                      {element.category}
+                                    </p>
+                                  </td>
+                                </tr>
+                              </table>
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -161,54 +166,56 @@ function Student() {
                         navigate(`${element._id}`);
                       }}
                     >
-                      <div className='shadow-indigo-500/40 p-5 sm:flex sm:justify-between'>
-                        <div className='border-2 border-gray-700 p-2 flex justify-center'>
-                          <img
-                            src={`http://localhost:5000/${element.image}`}
-                            alt='flower Image'
-                            className='rounded-full w-32 h-32 sm:w-40 sm:h-40 object-fill border-red-900 border-2'
-                          />
-                        </div>
-                        <div className='border-2 border-red-700 p-2'>
-                          <div className='text-center items-center'>
-                            <h1 className='font-semibold'>
-                              {element.fullName}
-                            </h1>
-                            <p className='pt-2 text-sm'>
-                              {`${element.address.slice(0, 10)}...`}
-                            </p>
+                      <div>
+                        <div className='shadow-lg shadow-indigo-500/40 p-2 sm:flex sm:justify-between border-2 mb-5'>
+                          <div className='p-2 flex justify-center'>
+                            <img
+                              src={`http://localhost:5000/${element.image}`}
+                              alt='flower Image'
+                              className='rounded-full w-32 h-32 sm:w-32 sm:h-w-32 object-fill'
+                            />
                           </div>
-                          <div className='flex justify-between text-center'>
-                            <table className='p-1 border text-sm'>
-                              <tr>
-                                <td className='border'>
-                                  Gender
-                                  <p className='pt-2 text-xs'>
-                                    {element.gender}
-                                  </p>
-                                </td>
-                              </tr>
-                            </table>
-                            <table className='p-1 border text-sm'>
-                              <tr>
-                                <td className=' border'>
-                                  class
-                                  <p className='pt-2 text-xs'>
-                                    {element.studentClass}
-                                  </p>
-                                </td>
-                              </tr>
-                            </table>
-                            <table className='p-1 border text-sm'>
-                              <tr>
-                                <td className='border'>
-                                  Gender
-                                  <p className='pt-2 text-xs'>
-                                    {element.category}
-                                  </p>
-                                </td>
-                              </tr>
-                            </table>
+                          <div className='p-2'>
+                            <div className='text-center items-center'>
+                              <h1 className='font-semibold'>
+                                {element.fullName}
+                              </h1>
+                              <p className='pt-2 text-sm'>
+                                {`${element.address.slice(0, 10)}...`}
+                              </p>
+                            </div>
+                            <div className='flex justify-between text-center'>
+                              <table className='p-1 border text-sm'>
+                                <tr>
+                                  <td className='border'>
+                                    Gender
+                                    <p className='pt-2 text-xs'>
+                                      {element.gender}
+                                    </p>
+                                  </td>
+                                </tr>
+                              </table>
+                              <table className='p-1 border text-sm'>
+                                <tr>
+                                  <td className=' border'>
+                                    class
+                                    <p className='pt-2 text-xs'>
+                                      {element.studentClass}
+                                    </p>
+                                  </td>
+                                </tr>
+                              </table>
+                              <table className='p-1 border text-sm'>
+                                <tr>
+                                  <td className='border'>
+                                    Gender
+                                    <p className='pt-2 text-xs'>
+                                      {element.category}
+                                    </p>
+                                  </td>
+                                </tr>
+                              </table>
+                            </div>
                           </div>
                         </div>
                       </div>
