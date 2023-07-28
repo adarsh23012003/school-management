92c6ee64c2e57371c9e1b5362c0f1a5e4e134839
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-28 13:23:34 +0530

    Login and SignUp route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from "../src/pages/About";
 import NoPage from "../src/pages/NoPage";
 import Student from "../src/pages/Student";
 import StudentDetails from "../src/pages/StudentDetails";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/student' element={<Student />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/student/:studentId' element={<StudentDetails />} />
         <Route path='*' element={<NoPage />} />
       </Routes>
