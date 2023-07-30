f6a1e14b62c7bbc87e7191ad1ab070c978e2e3b8
Adarsh Maurya <adarshmaurya2303@gmail.com>
2023-07-30 13:45:16 +0530

    login page styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,43 +22,45 @@ function Login() {
       });
   }
   return (
-    <div className='flex justify-center pt-5'>
-      <div className=' border border-red-400 p-7 rounded-md'>
-        <form action=''>
-          <div className='pb-3'>
-            <input
-              onChange={(e) => {
-                formData.email = e.target.value;
+    <div className='bg-gray-200 h-screen'>
+      <div className='absolute left-[50%] top-[50%] transform -translate-x-[50%] -translate-y-[50%]'>
+        <div className='shadow-red-400 shadow-md p-7 rounded-md'>
+          <form action=''>
+            <div className='pb-3'>
+              <input
+                onChange={(e) => {
+                  formData.email = e.target.value;
+                }}
+                required
+                className='border focus:border-2 focus:border-blue-400 w-auto sm:w-72 p-2 rounded-md focus:p-2.5 transition-all duration-300 ease-in-out outline-none'
+                placeholder='Enter email'
+                type='email'
+              />
+            </div>
+            <div className='pb-3'>
+              <input
+                onChange={(e) => {
+                  formData.password = e.target.value;
+                }}
+                required
+                className='border focus:border-2 focus:border-blue-400 w-auto sm:w-72 p-2 rounded-md focus:p-2.5 transition-all duration-300 ease-in-out outline-none'
+                placeholder='Enter password'
+                type='password'
+              />
+            </div>
+          </form>
+          <div className='flex justify-center pt-2'>
+            <button
+              className='p-1.5 px-4 sm:px-6  bg-blue-500 rounded-md hover:bg-blue-600'
+              onClick={() => {
+                if (formData.email && formData.password) {
+                  handelSubmit();
+                }
               }}
-              required
-              className='border focus:outline-blue-400 p-2 rounded-md'
-              placeholder='Enter email'
-              type='email'
-            />
+            >
+              Submit
+            </button>
           </div>
-          <div className='pb-3'>
-            <input
-              onChange={(e) => {
-                formData.password = e.target.value;
-              }}
-              required
-              className='border focus:outline-blue-400 p-2 rounded-md'
-              placeholder='Enter password'
-              type='password'
-            />
-          </div>
-        </form>
-        <div className='flex justify-center pt-2'>
-          <button
-            className='p-1.5 bg-blue-500 rounded-md hover:bg-blue-600'
-            onClick={() => {
-              if (formData.email && formData.password) {
-                handelSubmit();
-              }
-            }}
-          >
-            Submit
-          </button>
         </div>
       </div>
     </div>
